555ecacf284ce90e65ce703268b88c53b6384169
Mohdhar Noor <mohdhar.noor@gmail.com>
2014-11-08 19:31:38 -0500

    Added a shopping cart image

--- A3/application/views/header.php
+++ A3/application/views/header.php
@@ -1,4 +1,4 @@
 <div id="header">
 	<div id="signin" class="right">Sign in</div>
-	quickStore Header
+	<img class="header_img right" src="//localhost/estore/images/main/1415513928_icon-ios7-cart-outline-64.png" />
 </div>
