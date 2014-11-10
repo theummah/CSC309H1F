e0711a81949078b4a1347c026f032c07f585cba5
Mohdhar Noor <mohdhar.noor@gmail.com>
2014-11-10 14:13:00 -0500

    recommitted header

--- A3/application/views/header.php
+++ A3/application/views/header.php
@@ -1,10 +1,17 @@
-<div id="header">
+<?php
+/*<div id="header">
 	<span id="site_name" class="left">Our Store</span>
 	<a href=""><div id="signin" class="right">Sign in</div></a>
 	<img class="header_img right" src="//localhost/estore/images/main/1415514808_icon-ios7-cart-outline-48.png" />
+</div>*/
+?>
+<div id="header_container">
+<span id="header_text">
+quickStore
+</span>
 <?php
-//		if (isset($username)){
-//			echo "Username: ".$username;
-//		}
-	?>
+if (isset($username)){
+echo "Username: ".$username;
+}
+?>
 </div>
